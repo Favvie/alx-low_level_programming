a3d92aec181c6b33319f7d61ffd177f78690db43
Favvie <favourprecious3@gmail.com>
2022-03-13 16:50:34 -0700

    Print lowercase and uppercase letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,11 +3,28 @@
 /**
  * main - Etnry point
  *
- * Description -
+ * Description - Print lowercase and uppercase letters
  *
  * Return: 0
  */
 int main(void)
 {
+	char c;
+	char d;
+
+	c = 'a';
+	d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
 	return (0);
 }
