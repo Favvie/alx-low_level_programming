15cef30411e7934a5fc4cdcc8437d70b7d86263c
Favvie <favourprecious3@gmail.com>
2022-03-13 09:54:54 -0700

    Checks the last digit of a randome number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description - checks the last digit
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int c;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	c = n % 10;	
+	if (n % 10 > 5)
+		printf("Last digit of %d is %d and is greater than  5\n", n, c);
+	else if (n % 10 == 0)
+		printf("Last digit of %d is %d and is 0\n", n, c);
+	else if (n % 10 < 6 && n % 10 != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, c);
+	return (0);
+
+}
