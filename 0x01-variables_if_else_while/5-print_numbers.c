49b7593aefc0502d36b08f463b4164fcbe9fae87
Favvie <favourprecious3@gmail.com>
2022-03-13 17:10:44 -0700

    Print out digits in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description - Print numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+	return (0);
+}
