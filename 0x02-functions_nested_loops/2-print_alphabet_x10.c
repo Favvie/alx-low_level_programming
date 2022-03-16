58e3c5be8493ba0c18a4d5294e8c2cdd61bcfa57
Favvie <favourprecious3@gmail.com>
2022-03-16 05:18:27 +0100

    Task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet a-z followed by a new line 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	char c;
+
+	x = 0;
+	while (x < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
