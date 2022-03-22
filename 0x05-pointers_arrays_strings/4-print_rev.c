01480414a29e7ea8911a22c2f5165dc1df2d8e20
Favvie <favourprecious3@gmail.com>
2022-03-22 03:29:27 -0700

    Submit tasks

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - prints string to stdoutput in reverse
+ * @s: pointer to string
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i >= 0)
+	{
+		if (s[i] != '\0')
+			_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
