32b2f1aa5510c5f717c24a9fc39919fb2f505503
Favvie <favourprecious3@gmail.com>
2022-04-13 05:00:43 +0100

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - a struct about dog features
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: creates a struct
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
