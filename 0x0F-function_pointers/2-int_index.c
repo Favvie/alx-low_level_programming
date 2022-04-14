1683a3b666f0e91be7e4f6d2610fbded8abe1bc4
Favvie <favourprecious3@gmail.com>
2022-04-14 02:53:17 +0100

    task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,14 +9,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int count;
 
-	if (size <= 0) return (-1);
-	if (cmp != NULL)
-	{
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+
 		for (count = 0; count < size; count++)
 		{
 			if(cmp(array[count]))
 				return (count);
 		}
-	}
+	
 	return (-1);
 }
