a692ac2333c1f97c7957a458ba21c15ce91e127d
Favvie <favourprecious3@gmail.com>
2022-04-14 02:57:09 +0100

    task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,6 +3,11 @@
 
 /**
  * int_index - return the index of the first valid element
+ * @array: the array to search through
+ * @size: size of the array
+ * @cmp: compare function
+ *
+ * Return: index
  *
  */
 int int_index(int *array, int size, int (*cmp)(int))
@@ -12,11 +17,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
-		for (count = 0; count < size; count++)
-		{
-			if(cmp(array[count]))
-				return (count);
-		}
+	for (count = 0; count < size; count++)
+	{
+		if(cmp(array[count]))
+			return (count);
+	}	
 	
 	return (-1);
 }
