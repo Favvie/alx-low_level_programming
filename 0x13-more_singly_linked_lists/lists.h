67414c1eb3861e6fd6ab6e4ecc55818c6d07b13a
Favvie <favourprecious3@gmail.com>
2022-04-26 16:24:28 +0100

    print out the number of nodes in a list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -16,5 +19,6 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-
+size_t listint_len(const listint_t *h);
+int _putchar(char c);
 #endif
