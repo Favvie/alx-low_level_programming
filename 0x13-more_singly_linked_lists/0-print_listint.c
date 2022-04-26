93146d5c16245ff16f510d307e335d3904b8535a
Favvie <favourprecious3@gmail.com>
2022-04-26 15:42:20 +0100

    Rename folder

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_listint - print all the elements of a list
+ * @h: the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	/* the parameter passed in, is a pointer to the first element in the list */
+	size_t count = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h= h->next;
+		count++;
+	}
+	return count;
+}
