4b348fe2cb8523dc97787a239e0ccdab6c5ccad2
Favour Abangwu <66237754+Favvie@users.noreply.github.com>
2022-04-27 04:55:51 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -8,6 +6,7 @@
  *
  * Return: number of elements in a list
  */
+
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
